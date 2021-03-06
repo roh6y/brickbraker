b0fb388b1caf9201b695d7d84ffab654cbcfde6f
roh6y <rohitkrishnanj@gmail.com>
2021-03-06 11:43:06 +0530

    initial commit

---
+++ src/brickbreaker/Main.java
@@ -0,0 +1,20 @@
+package brickbreaker;
+	import javax.swing.JFrame;
+
+	public class Main {
+	    public static void main(String[] args)
+	    {
+	        JFrame obj = new JFrame();
+	        Gameplay gameplay = new Gameplay();
+	        obj.setBounds(10,10,700,600);
+	        obj.setTitle("Brick Breaker");
+	        obj.setResizable(false);
+	        obj.setVisible(true);
+	        obj.setLocationRelativeTo(null);
+	        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        obj.add(gameplay);
+
+	    }
+	}
+
+
